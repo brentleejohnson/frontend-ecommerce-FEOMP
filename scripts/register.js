1eb83c03dbd452d21d974595354bf5ab94370ee8
Brent Johnson <brentleejohnson73@gmail.com>
2021-08-27 16:23:12 +0200

    Register and login works

---
+++ scripts/register.js
@@ -0,0 +1,31 @@
+function register() {
+  // GETTING DATA FROM FORM
+  let full_name = document.querySelector("#full_name").value;
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+  console.log(full_name, email, password);
+
+  //   SEND DATA TO API
+  fetch("http://127.0.0.1:5000/users/", {
+    method: "POST",
+    body: JSON.stringify({
+      full_name,
+      email,
+      password,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res.status_code == 201);
+      {
+        document.querySelector("#error").innerHTML =
+          "You have successfully registered, please sign in to continue";
+        setTimeout(function () {
+          window.location = "./index.html";
+        }, 3000);
+      }
+    });
+}
