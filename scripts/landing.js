53782138d9bace55ef034ea377fe1ce2f704d5ce
brentleejohnson <brentleejohnson73@gmail.com>
2021-08-31 12:16:22 +0200

    Products successfully attached to user and displays

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,31 @@
+const user = JSON.parse(localStorage.getItem("user"));
+document.querySelector("#greeting").innerHTML = `Hello there ${user.full_name}`;
+
 function getProducts() {
-  const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user);
-  //   fetch("https://ecommerce-final-eomp.herokuapp.com/product/")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
+  fetch(`https://ecommerce-final-eomp.herokuapp.com/product/${user.user_id}`)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+
+      const products = res.data;
+      showProducts(products);
+    });
 }
+
 getProducts();
+
+function showProducts(products) {
+  let container = document.querySelector("#products");
+  container.innerHTML = ``;
+
+  products.forEach((product) => {
+    container.innerHTML += `
+        <div class="product-card">
+            <img class="product-image" src="${product.image}" alt="" />
+            <h3 class="product-title">${product.name}</h3>
+            <p class="product-description">${product.description}</p>
+            <p class="product-price">R${product.price}</p>
+        </div>
+        `;
+  });
+}
