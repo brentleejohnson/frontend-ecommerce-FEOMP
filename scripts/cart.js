5168c29718bc51e70c7bf1300edcacafd7ed30e4
brentleejohnson <brentleejohnson73@gmail.com>
2021-09-08 15:55:50 +0200

    Fixed delete from cart function

--- scripts/cart.js
+++ scripts/cart.js
@@ -15,9 +15,23 @@ function showCart(cart) {
         <img class="cart-image" src="${item.image}" alt="" />
         <h3 class="cart-name">${item.name}</h3>
         <p class="cart-price">${item.price}</p>
-        <button class="cart-button">Delete</button>
+        <button onclick="removeFromCart(this)" class="cart-button">Delete</button>
       </div>
       `;
   });
 }
 showCart(cart);
+
+// REMOVE FROM CART
+function removeFromCart(e) {
+  itemName = e.parentElement.querySelector(".cart-name").innerHTML;
+  console.log(itemName);
+  for (let x in cart) {
+    if (itemName == cart[x].name) {
+      cart.splice(x, 1);
+      console.log(cart);
+      window.localStorage["cart"] = JSON.stringify(cart);
+    }
+  }
+  showCart(cart);
+}
